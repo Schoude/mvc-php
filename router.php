a764afe3af6b86493d0a20b1f411cdcba53b31e9
Marc <marcbaque1311@gmail.com>
2024-02-08 17:28:43 +0100

    [ ADD ] finished lesson 15

---
+++ router.php
@@ -0,0 +1,29 @@
+<?php
+
+$path = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$routes = [
+  '/' => './controllers/index.php',
+  '/about' => './controllers/about.php',
+  '/contact' => './controllers/contact.php',
+];
+
+function routeToController(string $path, array $routes)
+{
+  if (array_key_exists($path, $routes)) {
+    require $routes[$path];
+  } else {
+    abort();
+  }
+}
+
+function abort(int $code = 404)
+{
+  http_response_code($code);
+
+  require "./views/{$code}.php";
+
+  die();
+}
+
+routeToController($path, $routes);
---
+++ views/404.php
@@ -0,0 +1,13 @@
+<?php require('partials/head.php') ?>
+
+<?php require('partials/nav.php') ?>
+
+<main>
+  <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+    <h1 class="text-3xl font-bold">404</h1>
+    <p>Page not found.</p>
+    <a href="/" class="block text-blue-500 underline mt-4">Try again here.</a>
+  </div>
+</main>
+
+<?php require('partials/footer.php') ?>
