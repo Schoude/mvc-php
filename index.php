bfa5b82c670ad1885f47c8e3cc4fcd39f8648e59
Marc <marcbaque1311@gmail.com>
2024-02-08 23:00:16 +0100

    [ ADD ] finished lesson 17

--- index.php
+++ index.php
@@ -2,4 +2,16 @@
 
 require './functions.php';
 
-require './router.php';
+// require './router.php';
+
+require './Database.php';
+
+$db = new Database();
+
+// PDO::FETCH_ASSOC returns only the associative array values ('id' => 1, 'name' => 'Test')
+// and not the indexed ([0] => 1, [1] => 'Test') values
+$posts = $db->query('select * from posts')->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+  echo "<li>{$post['title']}</li>";
+}
