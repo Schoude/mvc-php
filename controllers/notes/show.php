05c16df9606773b6f890d6bbdf5bd8df14f9b607
Marc <marcbaque1311@gmail.com>
2024-02-10 20:43:33 +0100

    [ UPDATE ] finish lesson 32

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -4,24 +4,47 @@ use Core\Database;
 
 /**
  * require() as a function saves the value returned from * a php file.
- * This is similar to 'export' in a JS module.
+ * This is similar to 'import' in a JS module.
  */
 $config = require(base_path('config.php'));
 $db = new Database($config['database']);
 
 $currentUserId = 1;
-$heading = 'Note Detail';
 
-$note = $db->query(
-  'select * from notes where id = :noteId',
-  [
-    ':noteId' => $_GET['id'],
-  ]
-)->findOrFail();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // First check if the user owns the note.
+  $note = $db->query(
+    'select * from notes where id = :noteId',
+    [
+      ':noteId' => $_POST['id'],
+    ]
+  )->findOrFail();
 
-authorize($note['user_id'] === $currentUserId);
+  authorize($note['user_id'] === $currentUserId);
 
-view('notes/show.view.php', [
-  'heading' => $heading,
-  'note' => $note,
-]);
+  // Delete the note of the user.
+  $db->query('delete from notes where id = :id', [
+    ':id' => $_POST['id'],
+  ]);
+
+  // Redirect to /notes
+  header('Location: /notes');
+
+  die();
+} else {
+  $heading = 'Note Detail';
+
+  $note = $db->query(
+    'select * from notes where id = :noteId',
+    [
+      ':noteId' => $_GET['id'],
+    ]
+  )->findOrFail();
+
+  authorize($note['user_id'] === $currentUserId);
+
+  view('notes/show.view.php', [
+    'heading' => $heading,
+    'note' => $note,
+  ]);
+}
--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -6,6 +6,11 @@
   <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
     <a href="/notes" class="text-blue-800 hover:underline">Go Back</a>
     <p class="mt-8"><?= htmlspecialchars($note['body']) ?></p>
+
+    <form method="POST" class="mt-4">
+      <input type="hidden" name="id" value="<?= $note['id'] ?>">
+      <button class="text-sm text-red-300 bg-gray-700 rounded-md px-3 py-2">Delete</button>
+    </form>
   </div>
 </main>
 
