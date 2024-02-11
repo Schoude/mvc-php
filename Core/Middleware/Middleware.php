4d2881ee977529f70ef34c9958cff61596708608
Marc <marcbaque1311@gmail.com>
2024-02-11 14:51:42 +0100

    [ UPDATE ] lesson 39

---
+++ Core/Middleware/Middleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Core\Middleware;
+
+class Middleware
+{
+  const MAP = [
+    'guest' => Guest::class,
+    'auth' => Auth::class,
+  ];
+
+  public static function resolve(?string $key)
+  {
+    if (!$key) {
+      return;
+    }
+
+    $middleware = static::MAP[$key] ?? false;
+
+    if (!$middleware) {
+      throw new \Exception("No matching middleware found for '$key'");
+    }
+
+    (new $middleware)->handle();
+  }
+}
--- Core/router.php
+++ Core/router.php
@@ -2,6 +2,7 @@
 
 namespace Core;
 
+use Core\Middleware\Middleware;
 use Core\Response;
 
 class Router
@@ -13,44 +14,56 @@ class Router
     $this->routes[] = [
       'uri' => $uri,
       'controller' => $controllerPath,
-      'method' => $requestMethod
+      'method' => $requestMethod,
+      'middleware' => null,
     ];
+
+    return $this;
   }
 
   public function get(string $uri, string $controllerPath)
   {
-    $this->add('GET', $uri, $controllerPath);
+    return $this->add('GET', $uri, $controllerPath);
   }
 
   public function post(string $uri, string $controllerPath)
   {
-    $this->add('POST', $uri, $controllerPath);
+    return $this->add('POST', $uri, $controllerPath);
   }
 
   public function delete(string $uri, string $controllerPath)
   {
-    $this->add('DELETE', $uri, $controllerPath);
+    return $this->add('DELETE', $uri, $controllerPath);
   }
 
   public function patch(string $uri, string $controllerPath)
   {
-    $this->add('PATCH', $uri, $controllerPath);
+    return $this->add('PATCH', $uri, $controllerPath);
   }
 
   public function put(string $uri, string $controllerPath)
   {
-    $this->add('PUT', $uri, $controllerPath);
+    return $this->add('PUT', $uri, $controllerPath);
+  }
+
+  public function only(string $key)
+  {
+    $this->routes[array_key_last($this->routes)]['middleware'] = $key;
+
+    return $this;
   }
 
   public function route(string $uri, string $requestMethod)
   {
     foreach ($this->routes as $route) {
       if ($route['uri'] === $uri && $route['method'] === strtoupper($requestMethod)) {
+        // Apply registered middleware
+        // Null checks are handled within the resolve method
+        Middleware::resolve($route['middleware']);
+
         return require base_path($route['controller']);
       }
     }
-
-    $this->abort();
   }
 
   protected function abort(int $code = Response::NOT_FOUND)
