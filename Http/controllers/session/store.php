5f641474a2fe46ff4b6cd700f73d8ad58d868fc0
Marc <marcbaque1311@gmail.com>
2024-02-11 19:32:38 +0100

    [ ADD ] finish lesson 42

--- Http/controllers/session/store.php
+++ Http/controllers/session/store.php
@@ -2,7 +2,7 @@
 
 use Core\App;
 use Core\Database;
-use Core\Validator;
+use Http\Forms\LoginForm;
 
 $email = $_POST['email'];
 $password = $_POST['password'];
@@ -10,20 +10,12 @@ $password = $_POST['password'];
 /** @var Database $db */
 $db = App::resolve(Database::class);
 
-// Vaildate inputs
-$errors = [];
+// Vaildate form inputs
+$form = new LoginForm();
 
-if (!Validator::email($email)) {
-  $errors['email'] = 'Please provide a valid email address.';
-}
-
-if (!Validator::string($password)) {
-  $errors['password'] = 'Please provide a valid password.';
-}
-
-if (!empty($errors)) {
+if (!$form->validate($email, $password)) {
   return view('session/create.view.php', [
-    'errors' => $errors,
+    'errors' => $form->errors(),
   ]);
 }
 
