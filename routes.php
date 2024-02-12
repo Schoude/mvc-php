021f823b919e5f3d60fee2eb061bec52b63e3124
Marc <marcbaque1311@gmail.com>
2024-02-12 12:37:50 +0100

    [ UPDATE ] route authentication

--- routes.php
+++ routes.php
@@ -8,17 +8,20 @@ $router->get('/contact', 'contact.php');
 $router->get('/notes', 'notes/index.php')->only('auth');
 // Note
 // View routes
-$router->get('/note', 'notes/show.php');
-$router->get('/note/create', 'notes/create.php');
-$router->get('/note/edit', 'notes/edit.php');
+$router->get('/note', 'notes/show.php')->only('auth');
+$router->get('/note/create', 'notes/create.php')->only('auth');
+$router->get('/note/edit', 'notes/edit.php')->only('auth');
 // Interaction routes
-$router->post('/note', 'notes/store.php');
-$router->patch('/note', 'notes/update.php');
-$router->delete('/note', 'notes/destroy.php');
+$router->post('/note', 'notes/store.php')->only('auth');
+$router->patch('/note', 'notes/update.php')->only('auth');
+$router->delete('/note', 'notes/destroy.php')->only('auth');
 
 // Auth
+// Register
 $router->get('/register', 'registration/create.php')->only('guest');
-$router->post('/register', 'registration/store.php');
+$router->post('/register', 'registration/store.php')->only('guest');
+// Login
 $router->get('/login', 'session/create.php')->only('guest');
 $router->post('/session', 'session/store.php')->only('guest');
+// Logout
 $router->delete('/session', 'session/destroy.php')->only('auth');
